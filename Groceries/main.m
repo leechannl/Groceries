a2eb450151e8b578e94840e0b579957719836594
leechannl <leechannl@gmail.com>
2014-05-27 20:19:32 +0800

    Initial Commit

---
+++ Groceries/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Groceries
+//
+//  Created by mm on 5/27/14.
+//  Copyright (c) 2014 mm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
